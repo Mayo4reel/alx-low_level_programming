ee0fee89963eca02a8a81d864d7d0c344b1341eb
mayo4reel <oyekunlemayo@gmail.com>
2022-12-08 16:59:48 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+int main(void)
+
+{
+
+
+		char displayed[52];
+			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+
+				puts(displayed);
+
+					return (0);
+
+}
